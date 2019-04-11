f09a1eaa1ddc4a9382384af78918579fd7f07a47
Collin Pham <me@collinpham.com>
2019-04-10 21:18:54 -0700

    updating styles of DonationBox

--- src/components/DonationBox/index.js
+++ src/components/DonationBox/index.js
@@ -1,2 +1,1 @@
-export * from './FlipCard';
 export { DonationBox } from './DonationBox';
--- src/views/Art/Art.js
+++ src/views/Art/Art.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
+import { DonationBox } from '../../components';
 import { Link } from 'react-router-dom';
 import './Art.css';
 
@@ -6,28 +7,42 @@ export class Art extends Component {
 	render() {
 		return(
 			<div className="Art">
-				<div className="Art-container">
-					<div className="Art-title">
-						<h2>
-							I am creating art that uses the browser as a medium.
-						</h2>
-					</div>
-					<div className="Art-links">
-						<ul>
-							<li>
-								<Link to="/art/light-cycle" className="Art-link">
-									light cycle
-								</Link>
-							</li>
-							<li>
-								<span className="Art-link-disabled">
-									more to come...
-								</span>
-							</li>
-						</ul>
-					</div>
-				</div>
+				<ArtBoard/>
+				<DonationBox/>
 			</div>
 		);
 	}
 }
+
+export const ArtBoard = () => {
+	const [showDonationBox, setShowDonationBox] = useState(false);
+
+	return (
+		<div className="Art-board-container" style={{top: showDonationBox ? 'calc(-100% + 64px)' : '0px'}}>
+			<div className="Art-board">
+				<div className="Art-title">
+					<h2>
+						I am creating art that uses the browser as a medium.
+					</h2>
+				</div>
+				<div className="Art-links">
+					<ul>
+						<li>
+							<Link to="/art/light-cycle" className="Art-link">
+								light cycle
+							</Link>
+						</li>
+						<li>
+							<span className="Art-link-disabled">
+								more to come...
+							</span>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div className="Art-donation" onClick={() => {setShowDonationBox(!showDonationBox)}}>
+				{showDonationBox ? 'x' : '$'}
+			</div>
+		</div>
+	);
+};
